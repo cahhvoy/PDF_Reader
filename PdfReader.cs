c6d5f2954b74e33d87abe2eeed931f17a2ace528
cahhvoy <cahhvoy@gmail.com>
2020-03-03 23:38:59 +0300

    Initial Commit

--- PdfReader.cs
+++ PdfReader.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PDF_Reader
 {
-    public partial class Form1 : Form
+    public partial class PdfReader : Form
     {
-        public Form1()
+        public PdfReader()
         {
             InitializeComponent();
         }
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace PDF_Reader
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PdfReader());
         }
     }
 }
